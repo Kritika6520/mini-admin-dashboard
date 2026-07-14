7607f64723336aeda69c12de106872e05566079d
Kritika Penta <kritikapenta15@gmail.com>
2026-07-14 17:36:44 +0530

    updated login.jsx
    trying to fix login issues

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 }
     try {
       const res = await axios.post(
-        "https://mini-admin-dashboard.onrender.com/api/users/",
+        "https://mini-admin-dashboard.onrender.com/api/auth/login",
         {
           email,
           password,
